b84b98f6d83a528e6457ee060a437a443656ab7c
nancydu317 <48107789+nancydu317@users.noreply.github.com>
2021-08-27 14:06:32 -0400

    Add files via upload
    updated files

---
+++ 9_pizza_generator_TEMPLATE.py
@@ -0,0 +1,26 @@
+# Random Pizza Generator Template
+
+import random
+
+# pizza lists
+crust = []  
+meat = [] 
+veggie = []
+surprise = []  # optional extra list
+
+# message to users
+print("Welcome to Random Pizza Generator! We have the following selections: ")
+# print lists for user
+
+#Ask user to press "Enter"
+input("Press Enter to generate a random pizza.") #dont need a variable because the user's input is not used else where in the code
+
+# generate a random index position
+
+# generate a random item for each list
+
+# message to users
+print("Your random pizza is:")
+print(selected_meat, "and", selected_veggie, selected_crust, "crust pizza")
+
+print("Enjoy!")
---
+++ my_weather_app.py
@@ -0,0 +1,1 @@
+# start your app here!
